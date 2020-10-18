3d88fe5d6cfc85f98d3463a7234db892adb2fe20
然Y <inie0722@gmail.com>
2020-10-18 19:53:25 +0800

    增加 atomic_struct

--- include/parallelism/atomic_struct.hpp
+++ include/parallelism/atomic_struct.hpp
@@ -5,132 +5,135 @@
 
 #include <atomic>
 
-namespace libcpp
+namespace mio
 {
-    namespace detail
+    namespace parallelism
     {
-        template <size_t>
-        struct atomic_struct_raw;
-        template <>
-        struct atomic_struct_raw<1>
+        namespace detail
         {
-            using type = uint8_t;
-        };
-        template <>
-        struct atomic_struct_raw<2>
-        {
-            using type = uint16_t;
-        };
-        template <>
-        struct atomic_struct_raw<3>
-        {
-            using type = uint32_t;
-        };
-        template <>
-        struct atomic_struct_raw<4>
-        {
-            using type = uint32_t;
-        };
-        template <>
-        struct atomic_struct_raw<5>
-        {
-            using type = uint64_t;
-        };
-        template <>
-        struct atomic_struct_raw<6>
-        {
-            using type = uint64_t;
-        };
-        template <>
-        struct atomic_struct_raw<7>
-        {
-            using type = uint64_t;
-        };
-        template <>
-        struct atomic_struct_raw<8>
-        {
-            using type = uint64_t;
-        };
-    } // namespace detail
-
-    template <typename T>
-    class atomic_struct
-    {
-    private:
-        static_assert(sizeof(T) < sizeof(uint64_t), "Struct is larger than 8 bytes");
-        using raw = detail::atomic_struct_raw<sizeof(T)>;
-
-        std::atomic<raw> data;
-
-        raw encode(const T &val)
-        {
-            raw ret;
-            memcpy(&ret, &val, sizeof(val));
-            return ret;
-        }
-
-        T decode(const raw &val)
-        {
-            T ret;
-            memcpy(&ret, &val, sizeof(val));
-            return ret;
-        }
-
-    public:
-        T operator=(const T &val)
-        {
-            return decode(data = encode(val));
-        }
-
-        bool is_lock_free() const
-        {
-            return data.is_lock_free();
-        }
-
-        void store(const T val, std::memory_order mo = std::memory_order_seq_cst)
-        {
-            data.store(encode(val), mo);
-        }
-
-        T load(std::memory_order mo = std::memory_order_seq_cst)
-        {
-            return decode(data.load(mo));
-        }
-
-        operator T()
-        {
-            return decode(data);
-        }
-
-        T exchange(const T val, std::memory_order mo = std::memory_order_seq_cst)
-        {
-            return decode(data.exchange(encode(val), mo));
-        }
-
-        bool compare_exchange_weak(T &expected, T desired, std::memory_order success = std::memory_order_seq_cst, std::memory_order failure = std::memory_order_seq_cst)
-        {
-            auto exp = encode(expected);
-            auto ret = data.compare_exchange_weak(exp, encode(desired), success, failure);
-
-            if (!ret)
+            template <size_t>
+            struct atomic_struct_raw;
+            template <>
+            struct atomic_struct_raw<1>
             {
-                expected = decode(exp);
+                using type = uint8_t;
+            };
+            template <>
+            struct atomic_struct_raw<2>
+            {
+                using type = uint16_t;
+            };
+            template <>
+            struct atomic_struct_raw<3>
+            {
+                using type = uint32_t;
+            };
+            template <>
+            struct atomic_struct_raw<4>
+            {
+                using type = uint32_t;
+            };
+            template <>
+            struct atomic_struct_raw<5>
+            {
+                using type = uint64_t;
+            };
+            template <>
+            struct atomic_struct_raw<6>
+            {
+                using type = uint64_t;
+            };
+            template <>
+            struct atomic_struct_raw<7>
+            {
+                using type = uint64_t;
+            };
+            template <>
+            struct atomic_struct_raw<8>
+            {
+                using type = uint64_t;
+            };
+        } // namespace detail
+
+        template <typename T>
+        class atomic_struct
+        {
+        private:
+            static_assert(sizeof(T) < sizeof(uint64_t), "Struct is larger than 8 bytes");
+            using raw = detail::atomic_struct_raw<sizeof(T)>;
+
+            std::atomic<raw> data;
+
+            raw encode(const T &val)
+            {
+                raw ret;
+                memcpy(&ret, &val, sizeof(val));
+                return ret;
             }
 
-            return ret;
-        }
-
-        bool compare_exchange_strong(T &expected, T desired, std::memory_order success = std::memory_order_seq_cst, std::memory_order failure = std::memory_order_seq_cst)
-        {
-            auto exp = encode(expected);
-            auto ret = data.compare_exchange_strong(exp, encode(desired), success, failure);
-
-            if (!ret)
+            T decode(const raw &val)
             {
-                expected = decode(exp);
+                T ret;
+                memcpy(&ret, &val, sizeof(val));
+                return ret;
             }
 
-            return ret;
-        }
-    };
-} // namespace libcpp
+        public:
+            T operator=(const T &val)
+            {
+                return decode(data = encode(val));
+            }
+
+            bool is_lock_free() const
+            {
+                return data.is_lock_free();
+            }
+
+            void store(const T val, std::memory_order mo = std::memory_order_seq_cst)
+            {
+                data.store(encode(val), mo);
+            }
+
+            T load(std::memory_order mo = std::memory_order_seq_cst)
+            {
+                return decode(data.load(mo));
+            }
+
+            operator T()
+            {
+                return decode(data);
+            }
+
+            T exchange(const T val, std::memory_order mo = std::memory_order_seq_cst)
+            {
+                return decode(data.exchange(encode(val), mo));
+            }
+
+            bool compare_exchange_weak(T &expected, T desired, std::memory_order success = std::memory_order_seq_cst, std::memory_order failure = std::memory_order_seq_cst)
+            {
+                auto exp = encode(expected);
+                auto ret = data.compare_exchange_weak(exp, encode(desired), success, failure);
+
+                if (!ret)
+                {
+                    expected = decode(exp);
+                }
+
+                return ret;
+            }
+
+            bool compare_exchange_strong(T &expected, T desired, std::memory_order success = std::memory_order_seq_cst, std::memory_order failure = std::memory_order_seq_cst)
+            {
+                auto exp = encode(expected);
+                auto ret = data.compare_exchange_strong(exp, encode(desired), success, failure);
+
+                if (!ret)
+                {
+                    expected = decode(exp);
+                }
+
+                return ret;
+            }
+        };
+    } // namespace parallelism
+} // namespace mio
