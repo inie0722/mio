ba46c3f2a26ce665f7203d6ec11efcca5fbe948d
然Y <inie0722@gmail.com>
2020-11-30 16:40:53 +0800

    增加客户端

--- include/mq/pair.hpp
+++ include/mq/pair.hpp
@@ -8,124 +8,175 @@ namespace mio
 {
     namespace mq
     {
-        template <typename Protocol>
-        class pair
+        namespace pair
         {
-        public:
-            typedef typename Protocol::socket_t socket_t;
-            typedef typename Protocol::acceptor_t acceptor_t;
-
-        private:
-            boost::asio::io_context &io_context_;
-            acceptor_t acceptor_;
-            socket_t socket_;
-
-            void accept()
+            template <typename Protocol>
+            class server
             {
-                if (!socket_.is_open())
+            public:
+                typedef typename Protocol::socket_t socket_t;
+                typedef typename Protocol::acceptor_t acceptor_t;
+
+            private:
+                boost::asio::io_context &io_context_;
+                acceptor_t acceptor_;
+                socket_t socket_;
+
+                void accept()
                 {
-                    acceptor_.accept(socket_);
+                    if (!socket_.is_open())
+                    {
+                        acceptor_.accept(socket_);
+                    }
                 }
-            }
 
-            void accept(boost::asio::yield_context yield)
-            {
-                if (!socket_.is_open())
+                void accept(boost::asio::yield_context yield)
                 {
-                    acceptor_.accept(socket_, yield);
+                    if (!socket_.is_open())
+                    {
+                        acceptor_.accept(socket_, yield);
+                    }
                 }
-            }
 
-        public:
-            pair(boost::asio::io_context &io_context) : io_context_(io_context), acceptor_(io_context), socket_(io_context)
+            public:
+                server(boost::asio::io_context &io_context) : io_context_(io_context), acceptor_(io_context), socket_(io_context)
+                {
+                }
+
+                void bind(const std::string &address)
+                {
+                    acceptor_.bind(address);
+                }
+
+                void write(const void *data, size_t size)
+                {
+                    accept();
+
+                    try
+                    {
+                        socket_.write(data, size);
+                    }
+                    catch (...)
+                    {
+                        socket_.close();
+                        write(data, size);
+                    }
+                }
+
+                void write(const void *data, size_t size, boost::asio::yield_context yield)
+                {
+                    accept(yield);
+
+                    try
+                    {
+                        socket_.write(data, size, yield);
+                    }
+                    catch (...)
+                    {
+                        socket_.close();
+                        write(data, size, yield);
+                    }
+                }
+
+                void read(void *data, size_t size)
+                {
+                    accept();
+
+                    try
+                    {
+                        socket_.read(data, size);
+                    }
+                    catch (...)
+                    {
+                        socket_.close();
+                        read(data, size);
+                    }
+                }
+
+                void read(void *data, size_t size, boost::asio::yield_context yield)
+                {
+                    accept(yield);
+
+                    try
+                    {
+                        socket_.read(data, size, yield);
+                    }
+                    catch (...)
+                    {
+                        socket_.close();
+                        read(data, size, yield);
+                    }
+                }
+
+                void close()
+                {
+                    socket_.close();
+                    acceptor_.close();
+                }
+
+                ~server()
+                {
+                    close();
+                }
+            };
+
+            template <typename Protocol>
+            class client
             {
-            }
+            public:
+                typedef typename Protocol::socket_t socket_t;
+                typedef typename Protocol::acceptor_t acceptor_t;
 
-            void bind(const std::string &address)
-            {
-                acceptor_.bind(address);
-            }
+            private:
+                boost::asio::io_context &io_context_;
+                socket_t socket_;
 
-            void connect(const std::string &address)
-            {
-                socket_.connect(address);
-            }
+            public:
+                client(boost::asio::io_context &io_context) : io_context_(io_context), socket_(io_context)
+                {
+                }
 
-            void connect(const std::string &address, boost::asio::yield_context yield)
-            {
-                socket_.connect(address, yield);
-            }
+                void connect(const std::string &address)
+                {
+                    socket_.connect(address);
+                }
 
-            void write(const void *data, size_t size)
-            {
-                accept();
+                void connect(const std::string &address, boost::asio::yield_context yield)
+                {
+                    socket_.connect(address, yield);
+                }
 
-                try
+                void write(const void *data, size_t size)
                 {
                     socket_.write(data, size);
                 }
-                catch (...)
-                {
-                    socket_.close();
-                    write(data, size);
-                }
-            }
 
-            void write(const void *data, size_t size, boost::asio::yield_context yield)
-            {
-                accept(yield);
-
-                try
+                void write(const void *data, size_t size, boost::asio::yield_context yield)
                 {
                     socket_.write(data, size, yield);
                 }
-                catch (...)
-                {
-                    socket_.close();
-                    write(data, size, yield);
-                }
-            }
 
-            void read(void *data, size_t size)
-            {
-                accept();
-
-                try
+                void read(void *data, size_t size)
                 {
                     socket_.read(data, size);
                 }
-                catch (...)
-                {
-                    socket_.close();
-                    read(data, size);
-                }
-            }
 
-            void read(void *data, size_t size, boost::asio::yield_context yield)
-            {
-                accept(yield);
-
-                try
+                void read(void *data, size_t size, boost::asio::yield_context yield)
                 {
                     socket_.read(data, size, yield);
                 }
-                catch (...)
+
+                void close()
                 {
                     socket_.close();
-                    read(data, size, yield);
                 }
-            }
 
-            void close()
-            {
-                socket_.close();
-                acceptor_.close();
-            }
+                ~client()
+                {
+                    close();
+                }
+            };
 
-            ~pair()
-            {
-                close();
-            }
-        };
-    } // namespace mq
+        } // namespace pair
+    }     // namespace mq
 } // namespace mio
--- test/test.cpp
+++ test/test.cpp
@@ -5,24 +5,24 @@
 #include "network/tcp.hpp"
 #include "interprocess/pipe.hpp"
 
-#include "mq/mq.hpp"
-#include "mq/rpc.hpp"
+#include "mq/pair.hpp"
 
 
 struct Protocol
 {
-    
+    /*
     typedef mio::interprocess::pipe::acceptor acceptor_t;
     typedef mio::interprocess::pipe::socket socket_t;
+    */
     
-    /*
     typedef mio::network::tcp::acceptor acceptor_t;
     typedef mio::network::tcp::socket socket_t;
-    */
+    
 };
 
 int main(void)
 {
+    /*
     mio::mq::service<Protocol, mio::mq::rpc::service> s(1);
 
     s.run("ipv4:0.0.0.0:9988");
@@ -32,11 +32,11 @@ int main(void)
         return true;
     });
 
-    auto aa = s.get_service<0>();
+    //auto aa = s.get_service<0>();
 
     sleep(1);
     mio::mq::client<Protocol, mio::mq::rpc::client> c(1);
-    c.on_connect([](void *socket, std::vector<char> & arg){
+    c.on_connect([](std::vector<char> & arg){
 
         char msg[] = "123456789.0";
         arg.resize(sizeof(msg));
@@ -48,5 +48,20 @@ int main(void)
     auto rpc = c.make_client<0>("ipv4:0.0.0.0:9988");
     while(1)
         sleep(10);
+        */
+    
+    boost::asio::io_context io_context_;
+    mio::mq::pair::server<Protocol> s(io_context_);
+    s.bind("tcp:127.0.0.1:9988");
+
+    char msg[] = "123456789.0";
+    while (1)
+    {
+        s.write(msg, sizeof(msg));
+        sleep(1);
+        //std::this_thread::yield();
+    }
+    
+    
     return 0;
 }
