eec38c76b9f812e3da3b9edbe4b7df08324166e6
然Y <inie0722@gamil.com>
2020-12-19 11:35:50 +0800

    添加 无锁分配器

---
+++ include/parallelism/allocator.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <stddef.h>
+#include <type_traits>
+
+#include "parallelism/aba_ptr.hpp"
+
+namespace mio
+{
+    namespace parallelism
+    {
+        template <typename T_>
+        class allocator
+        {
+        public:
+            using value_type = T_;
+            using size_type = size_t;
+            using difference_type = ptrdiff_t;
+            using propagate_on_container_move_assignment = std::true_type;
+
+        private:
+            union alignas(alignof(std::atomic<aba_ptr<void>>) > alignof(T_) ? alignof(std::atomic<aba_ptr<void>>) : alignof(T_)) node
+            {
+                std::atomic<aba_ptr<node>> next;
+                char data[sizeof(T_)];
+            };
+
+            std::atomic<aba_ptr<node>> free_list;
+
+        public:
+            allocator()
+            {
+                free_list = nullptr;
+            }
+
+            ~allocator()
+            {
+                while (free_list.load())
+                {
+                    aba_ptr<node> next = free_list.load()->next;
+
+                    delete free_list.load();
+
+                    free_list = next;
+                }
+            }
+
+            aba_ptr<T_> allocate()
+            {
+                aba_ptr<node> exp = free_list;
+                aba_ptr<node> ret;
+                do
+                {
+                    //没有可以节点了 就自己分配一个
+                    if (exp == nullptr)
+                    {
+                        ret = new node;
+                        return static_cast<aba_ptr<T_>>(ret);
+                    }
+
+                    ret = exp;
+                } while (!free_list.compare_exchange_weak(exp, exp->next));
+
+                return static_cast<aba_ptr<T_>>(ret);
+            }
+
+            void deallocate(aba_ptr<T_> p)
+            {
+                aba_ptr<node> n = p;
+                aba_ptr<node> exp = free_list;
+                do
+                {
+                    n->next = exp;
+                } while (!free_list.compare_exchange_weak(exp, n));
+            }
+        };
+    } // namespace parallelism
+} // namespace mio
