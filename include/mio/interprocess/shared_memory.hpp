fbf2c689d51c86385427354e9be5bcb66f160392
然Y <inie0722@gamil.com>
2020-12-21 21:38:07 +0800

    ...

--- include/mio/interprocess/shared_memory.hpp
+++ include/mio/interprocess/shared_memory.hpp
@@ -8,10 +8,11 @@ namespace mio
 {
     namespace interprocess
     {
-        using offset_ptr = boost::interprocess::offset_ptr<void, int64_t, uint64_t>;
+        template<typename T_>
+        using offset_ptr = boost::interprocess::offset_ptr<T_, int64_t, uint64_t>;
 
-        using managed_shared_memory = boost::interprocess::basic_managed_shared_memory<char, boost::interprocess::rbtree_best_fit<boost::interprocess::mutex_family, offset_ptr>, boost::interprocess::iset_index>;
+        using managed_shared_memory = boost::interprocess::basic_managed_shared_memory<char, boost::interprocess::rbtree_best_fit<boost::interprocess::mutex_family, offset_ptr<void>>, boost::interprocess::iset_index>;
 
-        using managed_mapped_file = boost::interprocess::basic_managed_mapped_file<char, boost::interprocess::rbtree_best_fit<boost::interprocess::mutex_family, offset_ptr>, boost::interprocess::iset_index>;
+        using managed_mapped_file = boost::interprocess::basic_managed_mapped_file<char, boost::interprocess::rbtree_best_fit<boost::interprocess::mutex_family, offset_ptr<void>>, boost::interprocess::iset_index>;
     }
 }
