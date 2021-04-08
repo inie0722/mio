aa85ebc00a8c7260ef3fc5f2693f5f42f2c626af
然Y <inie0722@gmail.com>
2021-04-08 23:05:14 +0800

    增加 对模板函数的支持

--- include/mio/metaprogram/rand.hpp
+++ include/mio/metaprogram/rand.hpp
@@ -2,4 +2,4 @@
 
 #include <mio/metaprogram/hash.hpp>
 
-#define MIO_RAND() mio::metaprogram::hash{__COUNTER__}(__TIME__)
+#define MIO_RAND() (static_cast<int64_t>(mio::metaprogram::hash{__COUNTER__ + mio::metaprogram::hash{}(__TIME__)}(__PRETTY_FUNCTION__)))
