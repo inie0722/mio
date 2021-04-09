b2d3eb9c83e53cfd0acdd662260231872f0e2687
然Y <inie0722@gmail.com>
2021-04-09 22:41:03 +0800

    支持 同时获取多个随机数

--- include/mio/metaprogram/rand.hpp
+++ include/mio/metaprogram/rand.hpp
@@ -1,5 +1,26 @@
 #pragma once
 
+#include <array>
+
 #include <mio/metaprogram/hash.hpp>
 
 #define MIO_RAND() (static_cast<int64_t>(mio::metaprogram::hash{__COUNTER__ + mio::metaprogram::hash{}(__TIME__)}(__PRETTY_FUNCTION__)))
+
+namespace mio
+{
+    namespace metaprogram
+    {
+        template <size_t N>
+        constexpr auto rand(uint64_t seed)
+        {
+            std::array<int64_t, N> ret = {};
+
+            for (size_t i = 0; i < N; i++)
+            {
+                ret[i] = static_cast<int64_t>(mio::metaprogram::hash{seed}(i));
+            }
+
+            return ret;
+        }
+    }
+}
