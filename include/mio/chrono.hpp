8290be171e18e25775f36810de49ae686ff081da
然Y <inie0722@gmail.com>
2021-12-03 13:44:53 +0800

    优化 时间转换操作

--- include/mio/chrono.hpp
+++ include/mio/chrono.hpp
@@ -6,6 +6,10 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/date_time.hpp>
 
 namespace mio
 {
@@ -16,6 +20,36 @@ namespace mio
             using namespace std::chrono;
             return duration_cast<nanoseconds>(system_clock::now().time_since_epoch());
         }
+
+        inline std::chrono::nanoseconds to_time(const std::string &time)
+        {
+            using namespace std::chrono_literals;
+            std::vector<std::string> strs[4];
+            boost::split(strs[0], time, boost::is_any_of(" "));
+
+            boost::split(strs[1], strs[0][0], boost::is_any_of("-"));
+            boost::posix_time::ptime end{boost::gregorian::date(atoll(strs[1].at(0).c_str()), atoll(strs[1].at(1).c_str()), atoll(strs[1].at(2).c_str()))};
+            boost::posix_time::time_duration diff = end - boost::posix_time::ptime(boost::gregorian::date(1970, 1, 1));
+            auto ret = std::chrono::nanoseconds(diff.total_nanoseconds()) - 8h;
+
+            if (strs[0].size() >= 2)
+            {
+                boost::split(strs[2], strs[0][1], boost::is_any_of(":"));
+                ret += std::stoull(strs[2][0]) * 1h;
+                ret += std::stoull(strs[2][1]) * 1min;
+                ret += std::stoull(strs[2][2]) * 1s;
+            }
+
+            if (strs[0].size() >= 3)
+            {
+                boost::split(strs[3], strs[0][2], boost::is_any_of(":"));
+                ret += std::stoull(strs[3][0]) * 1ms;
+                ret += std::stoull(strs[3][1]) * 1us;
+                ret += std::stoull(strs[3][2]) * 1ns;
+            }
+
+            return ret;
+        }
     } // namespace chrono
 
     inline std::string to_string(std::chrono::nanoseconds tm)
@@ -25,18 +59,16 @@ namespace mio
         using namespace std::chrono_literals;
 
         std::chrono::time_point<std::chrono::system_clock> tt(std::chrono::duration_cast<std::chrono::milliseconds>(tm));
-
-        unsigned long long all_ns = tm.count();
-        unsigned long long ms = (unsigned long long)(all_ns % 31536000000000000 % 86400000000000 % 3600000000000 % 60000000000 % 1000000000) / 1000000;
-        unsigned long long us = (unsigned long long)(all_ns % 31536000000000000 % 86400000000000 % 3600000000000 % 60000000000 % 1000000000 % 1000000) / 1000;
-        unsigned long long ns = (unsigned long long)all_ns % 31536000000000000 % 86400000000000 % 3600000000000 % 60000000000 % 1000000000 % 1000000 % 1000;
+        unsigned long long ms = (unsigned long long)(tm % 1s / 1ms);
+        unsigned long long us = (unsigned long long)(tm % 1ms / 1us);
+        unsigned long long ns = (unsigned long long)(tm % 1us / 1ns);
 
         std::stringstream ss;
         auto t = std::chrono::system_clock::to_time_t(tt);
-        ss << std::put_time(std::localtime(&t), "%F %T");
+        ss << std::put_time(std::localtime(&t), "%F %T ");
 
         char buf[128];
-        sprintf(buf, " %03llu:%03llu:%03llu", ms, us, ns);
+        sprintf(buf, "%03llu:%03llu:%03llu", ms, us, ns);
 
         ss << buf;
         return ss.str();
