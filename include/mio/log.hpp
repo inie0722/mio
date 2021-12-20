2b28d55237c0defb8f87448e2f02a4b8c37bc825
然Y <inie0722@gmail.com>
2021-12-20 15:17:55 +0800

    优化 log性能

--- include/mio/log.hpp
+++ include/mio/log.hpp
@@ -1,56 +1,101 @@
 #pragma once
 
-#include <functional>
+#include <stddef.h>
+#include <stdint.h>
 #include <atomic>
-#include <thread>
-
-#include <boost/interprocess/sync/interprocess_semaphore.hpp>
-#include <fmt/format.h>
+#include <tuple>
 
 #include <mio/parallelism/ring_queue.hpp>
 #include <mio/chrono.hpp>
+#include <mio/parallelism/utility.hpp>
+
+#include <fmt/format.h>
 
 namespace mio
 {
+    template <size_t QUEUE_SIZE = 1024, size_t BUFFER_SIZE = 65536>
     class log
     {
     private:
-        mio::parallelism::ring_queue<std::function<void()>> queue_;
-        boost::interprocess::interprocess_semaphore semaphore_;
-        std::atomic<bool> flag_;
-        std::thread thread_;
+        struct message
+        {
+            void *ptr;
+            void (*fun)(void *ptr);
+            size_t size;
+            std::atomic<uint64_t> *readable_limit;
+        };
+
+        class SPSC_buffer
+        {
+        private:
+            char data_[BUFFER_SIZE];
+            alignas(CACHE_LINE) std::atomic<uint64_t> writable_limit_ = 0;
+            alignas(CACHE_LINE) std::atomic<uint64_t> readable_limit_ = 0;
+
+        public:
+            message alloc(size_t size)
+            {
+                message ret;
+                ret.readable_limit = &readable_limit_;
+                auto writable_limit = writable_limit_.load();
+                auto r = BUFFER_SIZE - writable_limit % BUFFER_SIZE;
+                ret.size = r < size ? r + size : size;
+
+                auto writable_index = writable_limit + ret.size % BUFFER_SIZE;
+
+                //写满了等待 可写
+                while (BUFFER_SIZE - (writable_limit - readable_limit_) < ret.size)
+                    ;
+
+                ret.ptr = &data_[writable_index];
+                return ret;
+            }
+
+            size_t size() const
+            {
+                return writable_limit_ - readable_limit_;
+            }
+        };
+
+        SPSC_buffer buffer_;
+        inline static mio::parallelism::ring_queue<message> queue_{QUEUE_SIZE};
 
     public:
-        log(size_t n)
-            : queue_(n), semaphore_(0)
-        {
-            this->flag_ = true;
-            thread_ = std::thread([&]()
-                                  {
-                                      while (flag_ && !queue_.empty())
-                                      {
-                                          this->semaphore_.wait();
-
-                                          std::function<void()> val;
-                                          this->queue_.pop(val);
-                                          val();
-                                      }
-                                  });
-        }
+        log() = default;
 
         ~log()
         {
-
-            this->flag_ = false;
-            thread_.join();
+            while (this->size())
+                ;
         }
 
-        template <typename Stream, typename... Args>
-        void operator()(Stream &stream, const std::string &fmt, const Args &...args)
+        template <typename Stream, typename Format, typename... Args>
+        void operator()(Stream &stream, const Format &fmt, const Args &...args)
         {
-            queue_.push([=, &stream]()
-                        { stream << fmt::format(fmt, args...); });
-            semaphore_.post();
+            using args_t = std::tuple<Stream &, Format, Args...>;
+            auto msg = buffer_.alloc(sizeof(args_t));
+            new (msg.ptr) args_t(stream, fmt, args...);
+
+            msg.fun = [](void *ptr)
+            {
+                auto *args = reinterpret_cast<args_t *>(ptr);
+                std::get<Stream &>(*args) << fmt::format(std::get<Format>(*args), std::get<Args>(*args)...);
+            };
+
+            queue_.push(msg);
+        }
+
+        size_t size() const
+        {
+            return buffer_.size();
+        }
+
+        static void run_once()
+        {
+            message msg;
+            queue_.pop(msg);
+            msg.fun(msg.ptr);
+            msg.readable_limit += msg.size;
         }
     };
 }
