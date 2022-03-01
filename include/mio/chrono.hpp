5b97448f6e73b02abdba753a7a608321b6971e84
然Y <inie0722@gmail.com>
2022-03-01 11:32:26 +0800

    更新 时间转换格式

--- include/mio/chrono.hpp
+++ include/mio/chrono.hpp
@@ -24,31 +24,12 @@ namespace mio
         inline std::chrono::nanoseconds to_time(const std::string &time)
         {
             using namespace std::chrono_literals;
-            std::vector<std::string> strs[4];
-            boost::split(strs[0], time, boost::is_any_of(" "));
+            std::vector<std::string> strs;
+            boost::split(strs, time, boost::is_any_of("."));
 
-            boost::split(strs[1], strs[0][0], boost::is_any_of("-"));
-            boost::posix_time::ptime end{boost::gregorian::date(atoll(strs[1].at(0).c_str()), atoll(strs[1].at(1).c_str()), atoll(strs[1].at(2).c_str()))};
-            boost::posix_time::time_duration diff = end - boost::posix_time::ptime(boost::gregorian::date(1970, 1, 1));
-            auto ret = std::chrono::nanoseconds(diff.total_nanoseconds()) - 8h;
-
-            if (strs[0].size() >= 2)
-            {
-                boost::split(strs[2], strs[0][1], boost::is_any_of(":"));
-                ret += std::stoull(strs[2][0]) * 1h;
-                ret += std::stoull(strs[2][1]) * 1min;
-                ret += std::stoull(strs[2][2]) * 1s;
-            }
-
-            if (strs[0].size() >= 3)
-            {
-                boost::split(strs[3], strs[0][2], boost::is_any_of(":"));
-                ret += std::stoull(strs[3][0]) * 1ms;
-                ret += std::stoull(strs[3][1]) * 1us;
-                ret += std::stoull(strs[3][2]) * 1ns;
-            }
-
-            return ret;
+            tm tm_;
+            strptime(strs[0].c_str(), "%Y-%m-%d %H:%M:%S", &tm_);
+            return std::chrono::nanoseconds(std::chrono::seconds(mktime(&tm_))) + std::chrono::nanoseconds(std::stoull(strs[1], nullptr, 0));
         }
 
         class stopwatch
