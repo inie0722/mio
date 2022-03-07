102071ed7f59ac031f60e8b80b14e2583b681b8b
然Y <inie0722@gmail.com>
2022-03-07 14:48:05 +0800

    ...

--- include/mio/parallelism/utility.hpp
+++ include/mio/parallelism/utility.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file utility.hpp
+ * @author 然Y (inie0722@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #pragma once
 
 #include <cstddef>
