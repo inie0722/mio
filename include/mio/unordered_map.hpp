7c565a832f5ac6151b9e034da2fabfee3beedcb6
然Y <inie0722@gmail.com>
2022-03-07 13:11:34 +0800

    更新 注释

--- include/mio/unordered_map.hpp
+++ include/mio/unordered_map.hpp
@@ -1,10 +1,30 @@
+/**
+ * @file unordered_map.hpp
+ * @author 然Y (inie0722@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #pragma once
 
 #include <parallel_hashmap/phmap.h>
 
 namespace mio
 {
-    template <class Key, class Value,
+    /**
+     * @brief unordered_map 才用GitHub parallel_hashmap
+     * 
+     * @tparam Key 
+     * @tparam Value 
+     * @tparam Hash 
+     * @tparam Eq 
+     * @tparam Alloc
+     */
+    template <class Key, 
+            class Value,
               class Hash = phmap::priv::hash_default_hash<Key>,
               class Eq = phmap::priv::hash_default_eq<Key>,
               class Alloc = phmap::priv::Allocator<phmap::priv::Pair<const Key, Value>>>
--- include/mio/unordered_set.hpp
+++ include/mio/unordered_set.hpp
@@ -1,9 +1,27 @@
+/**
+ * @file unordered_set.hpp
+ * @author 然Y (inie0722@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-03-07
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #pragma once
 
 #include <parallel_hashmap/phmap.h>
 
 namespace mio
 {
+    /**
+     * @brief unordered_set 才用GitHub parallel_hashmap
+     *
+     * @tparam T
+     * @tparam Hash
+     * @tparam Eq
+     * @tparam Alloc
+     */
     template <class T,
               class Hash = phmap::priv::hash_default_hash<T>,
               class Eq = phmap::priv::hash_default_eq<T>,
